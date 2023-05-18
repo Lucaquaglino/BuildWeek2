dcf7bb458c6ac2416fdf22896bbcb6394053f3f9
lucawar <lucaguerra92@yahoo.it>
2023-05-18 10:31:21 +0200

    modifiche giovedi mattina

--- assets/js/artistPages.js
+++ assets/js/artistPages.js
@@ -435,3 +435,53 @@ function updatePlayButton(){
     buttonImage.src = 'assets/imgs/play.svg'
    }
 }
+
+//   array playlist finte
+const playlistNames = [
+    "Be The Young Seasons 1-5",
+    "Be The Young Seasons 6-8",
+    "persona di m*rda (gen-feb 2023)",
+    "Musical 2022",
+    "pippo, pluto e paperino (nov-dec 2022)",
+    "early stage emily syndrome (sett-ott 2022)",
+    "Be the young",
+    "'...' cit. Kimiko (lug-ago 2022)",
+    "saggio vibes 💃🩰",
+    "main character energy (mag-giu 2022)",
+    "that fucking mood 🔪☠️",
+    "VEE, CARLOTTA E GIACOMO VANNO A BOSIO",
+    "An Emily Winchester kind of mood 🔪🖕",
+    "landing page (mar-apr 2022)",
+    "2021 lol",
+    "cosa cazzo vuol dire questa affermazione (gen-feb 2022)",
+    "honey and glass (nov-dic 2021)",
+    "(Revenge) Training Arc 🦍",
+    "Lidia 🤝 Emily",
+    "minecraft e nintendo switch (sep-oct 2021)",
+    "silvano d'orba? I hardly know her (lug - ago 2021)",
+    "Culo 2021",
+    "Frah Quintale Mix",
+    "Francesco Guccini Mix",
+    "Lo Stato Sociale Mix",
+    "chapter 4/? (mag-giu 2021)",
+    "Strive School <> The Hunt Motivation",
+    "mi stavo dimenticando (mar-apr 2021)",
+    "high school musical 1,2,3",
+    "quanto trash cazzo",
+    "The 2020 Playlist",
+    "ma(ncanza) che cazzo ne so io (gen-feb 2021)",
+  ];
+
+
+for (let i = 0; i < playlistNames.length; i++) {
+  const element = playlistNames[i];
+  console.log(element)
+
+
+  const playlistItem = document.createElement("p");
+  playlistItem.classList.add("playlist");
+  playlistItem.textContent = element;
+const playlistContainer=document.getElementById("playlist")
+playlistContainer.appendChild(playlistItem);
+ 
+}
